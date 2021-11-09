9eec32e35ba1e16eaf3f8cb94e2608cab3a7da3b
looping_boy <guillaume.lotis@inventhys.com>
2021-11-09 22:59:04 +0100

    Start with a simple transparent blurred window

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Media.Effects;
+using System.Runtime.InteropServices;
+
+namespace Micka___Loop
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            //this.Effect = new BlurEffect();
+            WindowBlur.SetIsEnabled(this, true);
+
+            InitializeComponent();
+        }
+        
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
+
+        private void bouton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+
+    
+    enum AccentState
+    {
+        ACCENT_ENABLE_BLURBEHIND = 3
+    }
+
+    struct AccentPolicy
+    {
+        public AccentState AccentState;
+        public int AccentFlags;
+        public int GradientColor;
+        public int AnimationId;
+    }
+
+    struct WindowCompositionAttributeData
+    {
+        public WindowCompositionAttribute Attribute;
+        public IntPtr Data;
+        public int SizeOfData;
+    }
+
+    enum WindowCompositionAttribute
+    {
+        WCA_ACCENT_POLICY = 19
+    }
+}
